4e018defb728472ded2fe4b02942fe8d16859071
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-12-05 10:18:37 -0300

    Add encrypt password, session and compare password

--- src/modules/customers/infra/typeorm/entities/Customer.ts
+++ src/modules/customers/infra/typeorm/entities/Customer.ts
@@ -6,6 +6,9 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
 @Entity('customers')
 class Customer {
   @PrimaryGeneratedColumn('uuid')
@@ -34,6 +37,33 @@ class Customer {
 
   @UpdateDateColumn()
   updated_at: Date;
+
+  public async session(): Promise<string> {
+    return jwt.sign(
+      {
+        firstname: this.firstname,
+        lastname: this.lastname,
+        email: this.email,
+        id: this.id,
+      },
+      process.env.JWT_SECRET_TOKEN,
+      {
+        expiresIn: '1h',
+      },
+    );
+  }
+
+  public async encryptPassword(): Promise<void> {
+    this.password = await bcrypt.hash(
+      this.password,
+      Number(process.env.BCRYPT_SALT_ROUNDS),
+    );
+  }
+
+  public async comparePassword(password: string): Promise<boolean> {
+    const comparePass = await bcrypt.compare(password, this.password);
+    return comparePass;
+  }
 }
 
 export default Customer;
