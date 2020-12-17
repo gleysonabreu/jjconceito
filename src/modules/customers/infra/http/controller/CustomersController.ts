e9d6d31c3897291eeef3bbf3324271519c09b8df
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-12-17 13:10:22 -0300

    Fix temporary security on register customer/admin

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -7,15 +7,7 @@ import customerViews from '../views/customers.view';
 
 export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
-    const {
-      firstname,
-      lastname,
-      password,
-      email,
-      phone,
-      cpf,
-      level_access,
-    } = request.body;
+    const { firstname, lastname, password, email, phone, cpf } = request.body;
 
     const createCustomer = container.resolve(CreateCustomerService);
 
@@ -26,7 +18,6 @@ export default class CustomersController {
       email,
       phone,
       cpf,
-      level_access,
     });
 
     return response.status(201).json(customerViews.render(customer));
